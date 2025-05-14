73ab02631415f1e647cc60cc443071cbd095a399
jgmalacarne <57012704+jgmalacarne@users.noreply.github.com>
2025-05-14 13:53:32 -0400

    Still not working

--- init.R
+++ init.R
@@ -13,7 +13,7 @@ my_packages = c("bslib", "broom", "cli", "conflicted", "dbplyr",
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
+    install.packages(p, dependencies = TRUE)
   }
 }
 invisible(sapply(my_packages, install_if_missing))
