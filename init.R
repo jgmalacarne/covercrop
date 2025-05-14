2b7867b890cad122985d94efedb806cb08a78722
jgmalacarne <57012704+jgmalacarne@users.noreply.github.com>
2025-05-14 13:47:43 -0400

    trying to fix textshaping

--- init.R
+++ init.R
@@ -9,7 +9,7 @@ my_packages = c("bslib", "broom", "cli", "conflicted", "dbplyr",
                 "plotly", "purrr", "ragg", "readr", "readxl","rmarkdown", "reprex",
                 "rlang", "rstudioapi", "rvest", "shiny","shinyalert","shinyBS",
                 "shinydashboard","shinydashboardPlus","shinyjs","shinyWidgets",
-                "shinyvalidate","stringr", "tibble", "tidyr","kableExtra", "xml2", "tidyverse")
+                "shinyvalidate","stringr", "textshaping", "tibble", "tidyr","kableExtra", "xml2", "tidyverse")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
