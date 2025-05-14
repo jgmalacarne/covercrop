b89c84a947769e3fcbc34fa41c9148de91696028
jgmalacarne <57012704+jgmalacarne@users.noreply.github.com>
2025-05-14 12:11:48 -0400

    Updated packages in init.R

--- init.R
+++ init.R
@@ -2,7 +2,10 @@
 #
 # Example R code to install packages if not already installed
 #
-my_packages = c("package_name_1", "package_name_2", ...)
+my_packages = c("bslib", "dplyr", "ggplot2", "gplots", "heatmaply","htmlwidgets",
+                "plotly","readxl","rmarkdown","shiny","shinyalerts","shinyBS",
+                "shinydashboard","shinydashboardPlus","shinyjs","shinyWidgets","shinyvalidate","stringr","tidyverse","kableExtra")
+
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
