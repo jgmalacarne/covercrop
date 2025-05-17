f9a1b5fd3ea24b468de473cd55022774a70a2338
jgmalacarne <57012704+jgmalacarne@users.noreply.github.com>
2025-05-17 11:56:04 -0400

    Try again

--- init.R
+++ init.R
@@ -1,15 +1,30 @@
 # init.R
 #
-# Example R code to install packages if not already installed
+# Install packages if not already installed
 #
-my_packages = c("bslib", "broom", "cli", "conflicted", "dbplyr", 
-                "dplyr", "dtplyr", "forcats", "ggplot2", "gplots", 
-                "haven", "heatmaply", "hms", "htmlwidgets", "httr",
-                "jsonlite", "lubridate", "magrittr", "modelr", "pillar", 
-                "plotly", "purrr", "ragg", "readr", "readxl","rmarkdown", "reprex",
-                "rlang", "rstudioapi", "rvest", "shiny","shinyalert","shinyBS",
-                "shinydashboard","shinydashboardPlus","shinyjs","shinyWidgets",
-                "shinyvalidate","stringr", "textshaping", "tibble", "tidyr","kableExtra", "xml2", "tidyverse")
+my_packages = c("bslib",
+                "dplyr",
+                "forcats",
+                "ggplot2",
+                "gplots",
+                "heatmaply",
+                "htmlwidgets",
+                "plotly",
+                "purrr",
+                "readr",
+                "readxl",
+                "rmarkdown",
+                "shiny",
+                "shinyalert",
+                "shinyBS",
+                "shinydashboard",
+                "shinydashboardPlus",
+                "shinyjs",
+                "shinyWidgets",
+                "shinyvalidate",
+                "stringr",
+                "tibble",
+                "tidyr")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
--- run.R
+++ run.R
@@ -1,25 +1,6 @@
 # run.R
 library(shiny)
 
-# Example R code to install packages if not already installed
-#
-# my_packages = c("bslib", "broom", "cli", "conflicted", "dbplyr", 
-#                 "dplyr", "dtplyr", "forcats", "ggplot2", "gplots", 
-#                 "haven", "heatmaply", "hms", "htmlwidgets", "httr",
-#                 "jsonlite", "lubridate", "magrittr", "modelr", "pillar", 
-#                 "plotly", "purrr", "ragg", "readr", "readxl","rmarkdown", "reprex",
-#                 "rlang", "rstudioapi", "rvest", "shiny","shinyalert","shinyBS",
-#                 "shinydashboard","shinydashboardPlus","shinyjs","shinyWidgets",
-#                 "shinyvalidate","stringr", "textshaping", "tibble", "tidyr","kableExtra", "xml2", "tidyverse")
-# 
-# install_if_missing = function(p) {
-#   if (p %in% rownames(installed.packages()) == FALSE) {
-#     install.packages(p)
-#   }
-# }
-# invisible(sapply(my_packages, install_if_missing))
-
-
 port <- Sys.getenv('PORT')
 shiny::runApp(
   appDir = getwd(),
